809b5adda167edaf12c6a2218e12312eda6f2186
vayrynen123 <osku27@hotmail.com>
2020-02-15 09:57:16 +0200

    Runko saattaa toimia.

--- Arduino_Proto_1/Arduino_Proto_1.ino
+++ Arduino_Proto_1/Arduino_Proto_1.ino
@@ -1,4 +1,4 @@
-#include <Stepper.h>
+
 
 /*
 Library examples for TM1638.
@@ -18,26 +18,37 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <TM1638.h>
 
+#include <Stepper.h>
+
+const int stepsPerRevolution = 200;
+
+// initialize the stepper library on pins 7 through 10:
+Stepper myStepper1(stepsPerRevolution, 7, 8, 9, 10);
+
+// initialize the stepper library on pins 3 through 6:
+Stepper myStepper2(stepsPerRevolution, 3, 4, 5, 6);
+
+#include <TM1638.h>
 // define a module on data pin 12, clock pin 13 and strobe pin 11 
-TM1638 module(12, 13, 11);
+TM1638 module(12, 13, 11, true, 1);
 
 byte buttons, oldbuttons;
 
-int Players = 4;
-int Cards = 5;
-
 bool Deal = false;
 
-
+int Players = 4;
+int Cards = 5;
+int Dealt_Cards = 0;
 
 void setup() {
 
+
+  // set the speed at 60 rpm:
+  myStepper1.setSpeed(60);
+  myStepper2.setSpeed(60);
+  
   Serial.begin(9600);
-
-  oldbuttons = 0;
-
 
 
 }
@@ -48,8 +59,8 @@ void loop() {
 
     // display a variables at 1st and 5th position
   module.setDisplayDigit(Players, 0, false);
-  module.setDisplayDigit(Cards, 4, false);
-  
+  module.setDisplayDigit(Cards, 3, false);
+
 
   buttons = module.getButtons();
 
@@ -69,11 +80,11 @@ void loop() {
                                 Players = --Players;
                                 buttons = 0;
                                 break;
-                            case 16:
+                            case 8:
                                 Cards = ++Cards;
                                 buttons = 0;
                                 break;
-                            case 32:
+                            case 16:
                                 Cards = --Cards;
                                 buttons = 0;
                                 break;
@@ -89,11 +100,81 @@ void loop() {
 
   if (Deal == true)
   {
-    Serial.print (Players);
-    Serial.print (Cards);
-    Deal = false;
-    //Test    
-    
+    for (int i = 0; i < Cards; i++)
+      {
+        // step one revolution  in one direction:
+        Serial.println("myStepper2 clockwise");
+        myStepper2.step(stepsPerRevolution);
+        ++Dealt_Cards;
+        module.setDisplayToDecNumber(Dealt_Cards, 0, false);
+        //Serial.println (Players);
+        //Serial.println (Cards);  
+        delay(100);
+      }
+
+    Serial.println("myStepper1 clockwise");
+    myStepper1.step(stepsPerRevolution);
+    delay(100);
+    Serial.println("myStepper1 clockwise");
+    myStepper1.step(stepsPerRevolution);
+
+    for (int i = 0; i < Cards; i++)
+      {
+        // step one revolution  in one direction:
+        Serial.println("myStepper2 clockwise");
+        myStepper2.step(stepsPerRevolution);
+        ++Dealt_Cards;
+        module.setDisplayToDecNumber(Dealt_Cards, 0, false);
+        //Serial.println (Players);
+        //Serial.println (Cards);  
+        delay(100);
+      }
+
+    Serial.println("myStepper1 clockwise");
+    myStepper1.step(stepsPerRevolution);
+    delay(100);
+    Serial.println("myStepper1 clockwise");
+    myStepper1.step(stepsPerRevolution);
+
+    for (int i = 0; i < Cards; i++)
+      {
+        // step one revolution  in one direction:
+        Serial.println("myStepper2 clockwise");
+        myStepper2.step(stepsPerRevolution);
+        ++Dealt_Cards;
+        module.setDisplayToDecNumber(Dealt_Cards, 0, false);
+        //Serial.println (Players);
+        //Serial.println (Cards);  
+        delay(100);
+      }
+
+    Serial.println("myStepper1 clockwise");
+    myStepper1.step(stepsPerRevolution);
+    delay(100);
+    Serial.println("myStepper1 clockwise");
+    myStepper1.step(stepsPerRevolution);
+
+    for (int i = 0; i < Cards; i++)
+      {
+        // step one revolution  in one direction:
+        Serial.println("myStepper2 clockwise");
+        myStepper2.step(stepsPerRevolution);
+        ++Dealt_Cards;
+        module.setDisplayToDecNumber(Dealt_Cards, 0, false);
+        //Serial.println (Players);
+        ////Serial.println (Cards);   
+        delay(100);
+      }
+
+    Serial.println("myStepper1 clockwise");
+    myStepper1.step(stepsPerRevolution);
+    delay(100);
+    Serial.println("myStepper1 clockwise");
+    myStepper1.step(stepsPerRevolution);
+
+      Deal = false;
+      Dealt_Cards = 0;
+      
    }
 
   /*Serial.print (Players);
